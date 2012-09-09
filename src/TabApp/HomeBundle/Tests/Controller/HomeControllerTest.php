9a25cac105e11d228f035647caa28ee6fb962b65
Rob McGrail <rob@aichi.com>
2012-09-09 21:19:56 +1200

    Tidyup.

--- src/TabApp/HomeBundle/Tests/Controller/HomeControllerTest.php
+++ src/TabApp/HomeBundle/Tests/Controller/HomeControllerTest.php
@@ -4,7 +4,7 @@ namespace TabApp\HomeBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class HomeControllerTest extends WebTestCase
 {
     public function testIndex()
     {
